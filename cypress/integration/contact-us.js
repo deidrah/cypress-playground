37a127d10419365896dea49c9aaf1b9c12ad6de8
Deidrah <dominikastasik93@gmail.com>
2021-08-08 21:21:56 +0200

    add first test

---
+++ cypress/integration/contact-us.js
@@ -0,0 +1,16 @@
+/// <reference types="Cypress" />
+
+describe("Test Contact Us form via webdriverUni", () => {
+  it("Should be able to submit a successful submission via contact us form", () => {
+    cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+    //cy.get('#contact-us').click({force: true});
+    cy.get('[name="first_name"]').type("Joe");
+    cy.get('[name="last_name"]').type("Doe");
+    cy.get('[name="email"]').type("joedoe@gmail.com");
+    cy.get('textarea.feedback-input').type("Lorem ipsum dolor sit amet.");
+    cy.get('[type="submit"]').click();
+  });
+  it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
+    //cypress code
+  });
+});
