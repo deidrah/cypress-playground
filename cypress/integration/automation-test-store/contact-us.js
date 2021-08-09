5e25d97dafdbce4ef7dba1c5551408f1ed3206ee
Deidrah <dominikastasik93@gmail.com>
2021-08-09 22:42:46 +0200

    improve selectors and add assertions

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -2,11 +2,14 @@
 
 describe("Test Contact Us form via Automation Test Store", () => {
   it("Should be able to submit a successful submission via contact us form", () => {
-    cy.visit('https://www.automationteststore.com/');
-    cy.get('.info_links_footer > :nth-child(5) > a').click();
+    cy.visit("https://www.automationteststore.com/");
+    cy.get("a[href$='contact']").click();
+    //cy.xpath("//a[contains(@href, 'contact')]").click();
     cy.get('#ContactUsFrm_first_name').type("Joe");
-    cy.get('#ContactUsFrm_email').type("joedoe@gmail.com");
-    cy.get('#ContactUsFrm_enquiry').type("Lorem ipsum dolor sit amet");
-    cy.get('.col-md-6 > .btn').click();
+    cy.get('#ContactUsFrm_email').type("joe_blogs123@gmail.com");
+    cy.get('#ContactUsFrm_email').should('have.attr', 'name', 'email');
+    cy.get('#ContactUsFrm_enquiry').type("Do you provide additional discount on bulk orders?");
+    cy.get("button[title='Submit']").click();
+    cy.get('.mb40 > :nth-child(3)').should('have.text', 'Your enquiry has been successfully sent to the store owner!');
   });
 });
